4828d0ef0db2445df168880a1748f019e7aa51f2
Mohamed M. Hagag <m.hagag@dvit.me>
2014-12-16 20:32:57 +0200

    updates

--- invoice_discount/__openerp__.py
+++ invoice_discount/__openerp__.py
@@ -5,7 +5,11 @@
     'summary': "Show Discount Total and Total before Discount on Invoices. ",
     'description':"Show Discount Total and Total before Discount on Invoices.",
     'author': 'M.Hagag@DVIT.ME',
+<<<<<<< HEAD
+    'website': 'http://dvit.me',
+=======
     'website': 'http://www.dvit.me',
+>>>>>>> afe9b26a7f70375d881b631bdff7ded586a0e0c2
     'depends': ['account_voucher'],
     'data': [
         'discount_view.xml',
