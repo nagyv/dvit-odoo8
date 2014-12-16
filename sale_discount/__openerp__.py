111dbbc5052827465e29e3a3242552df49f5269e
Mohamed M. Hagag <m.hagag@dvit.me>
2014-12-16 20:26:06 +0200

    uptades

---
+++ sale_discount/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale Discount',
+    'version': '1.0',
+    'category': 'Sales Management',
+    'summary': 'Show discount total and total before discount on sales orders.',
+    'description':"Show Discount Total and Total before Discount on Sales Orders.",
+    'author': 'M.Hagag@DVIT.ME',
+    'website': 'https://www.dvit.me',
+    'depends': ['sale','invoice_discount'],
+    'data': ['discount_view.xml',],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
