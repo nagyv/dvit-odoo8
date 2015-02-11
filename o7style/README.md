7405355616f855ed6af341e09882c9d9b6d5d434
Mohamed M. Hagag <m.hagag@dvit.me>
2015-02-11 22:20:54 +0200

    new module o7style - v.7 style

---
+++ o7style/README.md
@@ -0,0 +1,4 @@
+dvit-odoo8
+===========
+
+OpenERP 7 style for top menus .
---
+++ o7style/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OpenERP 7 style',
+    'version': '1.0',
+        'sequence': 1,
+    'category': 'Web',
+    'summary': '',
+    'description':"",
+    'author': 'M.Hagag@DVIT.ME',
+    'website': 'https://www.dvit.me',
+    'depends': ['web',
+                ],
+    'data': ['views/webclient_templates.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
