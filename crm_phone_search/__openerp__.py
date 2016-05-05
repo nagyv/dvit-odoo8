8c6bade8a6c20ab020f847172149bdff52555ebd
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2016-05-05 20:19:32 +0200

    New module crm phone search

---
+++ crm_phone_search/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'CRM search by phone',
+    'version': '8.0.1.0',
+    'category': 'CRM',
+    'summary': 'add search by phone filters on partners, leads / oppos.',
+    'author': 'DVIT',
+    'website': 'https://www.dvit.me',
+    'depends': ['crm',],
+    'data': ['views.xml'],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
