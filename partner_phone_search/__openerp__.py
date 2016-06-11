da9f2da661e7b61d567aaa1668036c383b713e8a
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2016-06-11 15:44:02 +0200

    new module partner_phone_search

---
+++ partner_phone_search/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Search partners by phone',
+    'version': '8.0.1.0',
+    'category': 'Tools',
+    'summary': 'search partners by phone in the normal *name* search.',
+    'author': 'DVIT',
+    'website': 'https://www.dvit.me',
+    'depends': ['base', ],
+    'data': ['views.xml'],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
