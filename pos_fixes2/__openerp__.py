dfe02ef31b5e5fb18330fd97439a9172cfaec900
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2016-07-27 04:35:16 +0200

    IMP: process each order on creation to avoid a big performance hit

---
+++ pos_fixes2/__openerp__.py
@@ -0,0 +1,36 @@
+{
+    'name': 'POS Fixes',
+    'summary': 'Point of Sale Fixes',
+    'description': """
+    This module now deppends on IngAdhoc's product_pack.
+
+    https://github.com/ingadhoc/product/tree/8.0/product_pack
+
+    Fixes included:
+
+    - Fix POS invoices payments by reconciling them automatically on POS session closing to get them paid.
+
+    - Complete the anglo-saxon journal entries missing in POS.
+
+    *ToDo*:
+
+        - Handle UoS convertion ex. selling in grams while product UoS is KG .
+     """,
+    'version': '8.0.1.1',
+    'category': 'Point of Sale',
+    'author': 'DVIT.me',
+    'website': 'http://dvit.me',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_anglo_saxon',
+        'account',
+        'account_voucher',
+        'point_of_sale',
+        'product_pack_pos',
+    ],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
