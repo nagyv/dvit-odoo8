152aade3042814965a8a2cb28b2dbb2cf81b72e4
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2016-09-25 14:49:32 +0200

    fix selling stockable prods index out of range error and other enhancements

--- contract_sale_task/models.py
+++ contract_sale_task/models.py
@@ -9,13 +9,15 @@ class saleOrder(models.Model):
 
 	client_order_ref = fields.Char(string="Reference/Description", required=True)
 	def action_button_confirm(self, cr, uid, ids, context=None):
-		old_action_button_confirm = super(saleOrder, self).action_button_confirm(cr, uid, ids, context)
-		project_task_obj = self.pool.get('project.task')
-		sale_order = self.browse(cr, uid, ids[0])
-		order_lines_for = sale_order.order_line
-		for order in order_lines_for:
-			task_id = project_task_obj.search(cr, uid, [('sale_line_id','=', order.id)])
-			task_record = project_task_obj.browse(cr, uid, task_id)
-			task_record[0].name = task_record[0].name + " " + "[" + sale_order.client_order_ref + "]"
-		return old_action_button_confirm
+		res = super(saleOrder, self).action_button_confirm(cr, uid, ids, context)
+		task_obj = self.pool.get('project.task')
+		orders = self.browse(cr, uid, ids)
+		# order_lines_for = sale_order.order_line
+		for order in orders:
+			for line in order.order_line:
+				task_ids = task_obj.search(cr, uid, [('sale_line_id','=', line.id)])
+				tasks = task_obj.browse(cr, uid, task_ids)
+				for task in tasks:
+					task.name = task.name + " " + "[" + order.client_order_ref + "]"
+		return res
 
